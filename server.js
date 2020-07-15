7fe0c36650bb591521e9e7439730e73f4fb50d03
lingyenlee <lingyenlee@gmail.com>
2020-07-15 11:51:31 +1000

    connect to mongo

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+dotenv.config()
+
+const db = process.env.DB_URL
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('mongoDB connected'))
+  .catch(err => console.log(err))
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
