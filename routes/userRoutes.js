667b56326ca4f3bd0693d36ab44bc40891b2f4f4
lingyenlee <lingyenlee@gmail.com>
2020-07-15 14:03:28 +1000

    add validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import User from '../models/userModel'
+import { check, validationResult } from 'express-validator'
 
 
 const router = express.Router()
@@ -9,42 +10,64 @@ const router = express.Router()
 // @desc Create a new user
 // @access Public
 
-router.post("/", async (req, res) => {
+router.post("/", [
 
-    const { firstName, lastName, email, password } = req.body
+    // username must be an email
+    check("firstName", "firstName is required")
+        .not()
+        .isEmpty(),
 
-    // Simple validation
-    if (!firstName || !lastName || !email || !password) {
-        return res.status(400).json({ msg: "Please enter all fields" });
-    }
+    check("lastName", "lastName is required")
+        .not()
+        .isEmpty(),
 
-    try {
+    check('email', "Please include a valid email.").isEmail(),
+    // password must be at least 5 chars long
+    check('password').isLength({ min: 6 })
+],
+    async (req, res) => {
 
-        //check if email exists
-        let user = await User.findOne({ email })
+        const errors = validationResult(req)
 
-        //if user email exist, send a message
-        if (user) {
-            return res.status(400).send({ msg: "Email already exists." })
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
         }
-        // or if email not exist, create a new user 
-        user = new User({
-            firstName,
-            lastName,
-            email,
-            password
-        })
-
-        await user.save()
-        res.status(400).send({ msg: "User saved" })
-
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ msg: "Internal server error"})
-    }
 
 
-})
+        const { firstName, lastName, email, password } = req.body
+
+        // Simple validation
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ msg: "Please enter all fields" });
+        }
+
+        try {
+
+            //check if email exists
+            let user = await User.findOne({ email })
+
+            //if user email exist, send a message
+            if (user) {
+                return res.status(400).send({ msg: "Email already exists." })
+            }
+            // or if email not exist, create a new user 
+            user = new User({
+                firstName,
+                lastName,
+                email,
+                password
+            })
+
+            await user.save()
+            res.status(400).send({ msg: "User saved" })
+
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).send({ msg: "Internal server error" })
+        }
+
+
+    })
 
 
 module.exports = router
