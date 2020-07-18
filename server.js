8bf67c865b14d7ab162dd1e828d4c001f31ca57d
Ross Baker <rossbaker32@gmail.com>
2020-07-18 20:47:19 +1000

    Add validation middleware
    This middleware returns an array of errors encountered by express validator

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRoutes from './routes/userRoutes'
-import categoriesRoutes from './routes/categoriesRoutes'
-import authRoutes from './routes/authRoutes'
-import budgetsRoutes from './routes/budgetsRoutes'
-import transactionRoutes from './routes/transactionRoutes'
-// import expressValidator from 'express-validator'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import categoriesRoutes from "./routes/categoriesRoutes";
+import authRoutes from "./routes/authRoutes";
+import budgetsRoutes from "./routes/budgetsRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import { expressValidator } from "express-validator";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// app.use(expressValidator())
+app.use(expressValidator());
 
-dotenv.config()
+dotenv.config();
 
-const db = process.env.DB_URL
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-})
-  .then(() => console.log('mongoDB connected'))
-  .catch(err => console.log(err))
+const db = process.env.DB_URL;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("mongoDB connected"))
+  .catch((err) => console.log(err));
 
 //use routes
-app.use("/users", userRoutes)
-app.use("/auth", authRoutes)
-app.use('/categories', categoriesRoutes)
-app.use("/budgets", budgetsRoutes)
-app.use("/transactions", transactionRoutes)
+app.use("/users", userRoutes);
+app.use("/auth", authRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/budgets", budgetsRoutes);
+app.use("/transactions", transactionRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
