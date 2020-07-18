b105b94d87c8a8f94c4e579de4a4fe63ad145b58
Ross Baker <rossbaker32@gmail.com>
2020-07-18 22:10:38 +1000

    refactor categories controller

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,17 +3,15 @@ import { body, validationResult, param } from "express-validator";
 
 export const createCategory = async (req, res) => {
   try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
     const { name, color, type } = req.body;
 
     const user = await User.findById(req.user.id);
 
-    if (user.categories.find((category) => category.name.toLowerCase() === name.toLowerCase())) {
+    if (
+      user.categories.find(
+        (category) => category.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
       return res.status(409).json({ msg: "Category already exists" });
     }
 
@@ -28,34 +26,35 @@ export const createCategory = async (req, res) => {
     return res.status(201).json(user.categories[user.categories.length - 1]);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ msg: "Internal server error" });
   }
 };
 
 export const getCategories = async (req, res) => {
   try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: error.array() });
-    }
-
     const user = await User.findById(req.user.id);
     return res.status(200).json(user.categories);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ msg: "Internal server error" });
+  }
+};
+
+export const getCategory = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    const category = user.categories.id(req.params.categoryId);
+
+    if (!category) return res.status(404).json({ msg: "category not found" });
+    return res.status(200).json(user.categories.id(req.params.categoryId));
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "Internal server error" });
   }
 };
 
 export const updateCategory = async (req, res) => {
   try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: error.array() });
-    }
-
-    const { name, color, type } = req.body;
-
     const user = await User.findById(req.user.id);
     const category = user.categories.id(req.params.categoryId);
 
@@ -63,25 +62,18 @@ export const updateCategory = async (req, res) => {
       return res.status(404).json({ msg: "Category not found" });
     }
 
-    if (name) category.name = name;
-    if (color) category.color = color;
-    if (type) category.type = type;
-    user.save();
+    await category.set(req.body);
+    await user.save();
 
     return res.status(200).json(category);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ msg: "Internal server error" });
   }
 };
 
 export const deleteCategory = async (req, res) => {
   try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: error.array() });
-    }
-    
     const user = await User.findById(req.user.id);
     const category = user.categories.id(req.params.categoryId);
 
@@ -94,10 +86,11 @@ export const deleteCategory = async (req, res) => {
     return res.status(200).json(category);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ msg: "Internal server error" });
   }
 };
 
-export const validate = (method) => {
+export const validationRules = (method) => {
   switch (method) {
     case "createCategory":
       return [
@@ -106,7 +99,10 @@ export const validate = (method) => {
         body("type").not().isEmpty().isIn(["income", "expense"]),
       ];
     case "updateCategory":
-      return [param("categoryId").exists()];
+      return [
+        param("categoryId").exists(),
+        body("type").not().isEmpty().isIn(["income", "expense"]),
+      ];
     case "deleteCategory":
       return [param("categoryId").exists()];
   }
--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -4,10 +4,12 @@ import auth from '../middleware/auth'
 import {
   createCategory,
   getCategories,
+  getCategory,
   updateCategory,
   deleteCategory,
-  validate
+  validationRules
 } from '../controllers/categoriesController'
+import { validate } from '../middleware/validate'
 
 dotenv.config();
 
@@ -16,18 +18,23 @@ const router = express.Router()
 // @route POST /categories
 // @desc Create a new category
 // @access private
-router.post('/', auth, validate('createCategory'), createCategory)
+router.post('/', auth, validationRules('createCategory'), validate, createCategory)
 
 // @route GET /categories
 // @desc Get a list of categories
 // @access private
 router.get('/', auth, getCategories)
 
+// @route GET /categories/categoryId
+// @desc Get a list of categories
+// @access private
+router.get('/:categoryId', auth, getCategory)
+
 // @route PUT/PATCH /categories
 // @desc Edit an existing category
 // @access private
-router.put('/:categoryId', auth, updateCategory)
-router.patch('/:categoryId', auth, updateCategory)
+router.put('/:categoryId', auth, validationRules('updateCategory'), validate, updateCategory)
+router.patch('/:categoryId', auth, validationRules('updateCategory'), validate, updateCategory)
 
 // @route DELETE /categories
 // @desc Delete an existing category
