f37ddd68f5d778bc101716d2d308d3e29c7b6179
lingyenlee <lingyenlee@gmail.com>
2020-07-26 14:04:00 +1000

    add password reset route

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,6 +61,46 @@ export const loginUser = async (req, res) => {
   }
 };
 
+export const changePassword = async (req, res) => {
+
+  try {
+    const { currentPassword, password } = req.body
+    // console.log("currentPassord", currentPassword)
+    // console.log("password", password)
+    // console.log("user", user)
+    //find user by id
+    let user = await User.findById(req.params.id)
+
+    //if no user return error message
+    if (!user) return res.status(404).json({ msg: 'User not found' })
+
+    //validate user if with decoded token
+    if (user._id.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not authorized" })
+    }
+
+    //if user and user id match, match password
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ msg: 'invalid credentials' });
+    }
+
+    //geneate salt of length 10 and save to salt
+    const salt = await bcrypt.genSalt(10);
+
+    //hash new password with salt
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+   return res.status(200).json(user);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ msg: 'Internal server error' });
+  }
+}
+
 export const validationRules = (method) => {
   switch (method) {
     case 'userLogin': {
@@ -71,6 +111,11 @@ export const validationRules = (method) => {
         // password must be at least 6 chars long
         body('password').isLength({ min: 6 }),
       ];
+    }
+    case 'changePassword': {
+      return [
+        body('password').isLength({ min: 6 })
+      ]
     }
   }
 };
